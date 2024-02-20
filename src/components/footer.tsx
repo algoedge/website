5d12a01d64a9e632f3bb2673551236908ff8981e
algoedgeak <arpitkumar@algoedge.co>
2024-02-20 12:10:34 +0530

    fixing footer layout issues

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
           <Logo />
         </div>
         <div>
-          <div className="flex justify-between text-xs">
+          <div className="flex flex-col justify-between text-xs md:flex-row ">
             <div>
               <p>&copy; {new Date().getFullYear()} Algoedge Technologies. All Rights Reserved. </p>
               <p className="mt-2">D-35, Chikhli Road, Tulsi Park Society, Vyara, Gujarat, India - 394650</p>
             </div>
 
-            <ul className="inline-flex space-x-4">
+            <ul className="inline-flex space-x-4 mt-5 md:mt-0">
               <li>
                 <Link href="/support">Support</Link>
               </li>
